53c018a05fb12887cd05a007e452f741824fb6b0
Joaquin Manuel Gonzalez <113261472+JoaquinManuelGonzalez@users.noreply.github.com>
2024-04-04 17:40:34 -0300

    Delete ejercicios_resueltos/ejercicio_cinco directory

--- ejercicios_resueltos/ejercicio_cinco/ej5_media_player/src/main/java/ar/edu/unlp/info/oo2/ej5_media_player/Media.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unlp.info.oo2.ej5_media_player;
-
-public abstract class Media {
-	
-	public abstract void play();
-	
-}
--- ejercicios_resueltos/ejercicio_cinco/ej5_media_player/src/main/java/ar/edu/unlp/info/oo2/ej5_media_player/VideoStream.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.unlp.info.oo2.ej5_media_player;
-
-public class VideoStream {
-	
-	public void reproduce() {
-		System.out.println("The Beatles - Now And Then (Official Music Video)");
-	}
-}
